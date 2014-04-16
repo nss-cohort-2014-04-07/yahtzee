1d69b0a92bc1ed4b704a0b7ee37ee95525d4b912
Chyld Medford <chyld.medford@gmail.com>
2014-04-16 10:47:25 -0500

    part 2

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -5,12 +5,34 @@
 
   var currentUser = 0;
   var currentRoll = 3;
+  var frozen;
+  var numDice;
 
   function init(){
     $('#add').click(add);
     $('.arrow').click(arrow);
     $('body').keydown(move);
     $('#add-score').click(addScore);
+    $('#roll').click(roll);
+    $('.dice').click(freeze);
+
+    numDice = $('.dice').length;
+    frozen = $('.frozen').length;
+  }
+
+  function freeze(){
+    $(this).toggleClass('frozen');
+  }
+
+  function roll(){
+    var $dice = $('.dice:not(.frozen)');
+    var count = $dice.length;
+
+    for(var i = 0; i < count; i++){
+      var num = Math.floor(Math.random() * 6) + 1;
+      var dice = $dice[i];
+      $(dice).attr('src', './media/' + num + '.png');
+    }
   }
 
   function addScore(event){
